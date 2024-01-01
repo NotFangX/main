cc7cf2781d75e817e95a9baa602856486fa33c5a
NotFangX <113139516+NotFangX@users.noreply.github.com>
2024-01-01 15:11:21 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const discordServer = document.getElementById('discordBTN');
+
+
+discordServer.addEventListener('click', function(){
+    window.location.href = "https://discord.gg/HuuX867SxH";
+})
